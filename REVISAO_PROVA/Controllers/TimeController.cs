d278d03334ed0b557beab3166cf1a457333923d9
Daniel Ferreira <danielteodoro2010@hotmail.com>
2020-09-22 16:26:49 -0300

    primeira versão

---
+++ REVISAO_PROVA/Controllers/TimeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REVISAO_PROVA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Listagem de times");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("Listagem de um time");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Model.Time time)
+        {
+            return Ok("Time cadastrado!!!");
+        }
+
+
+    }
+}
---
+++ REVISAO_PROVA/Model/Time.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REVISAO_PROVA.Model
+{
+    public class Time
+    {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string Estado { get; set; }
+        public int AnoCriação { get; set; }
+        public string bandeira { get; set; }
+        public string mascote { get; set; }
+        public int QtdeTitulosBR { get; set; }
+        public int QtdeTituloCopaBr { get; set; }
+        public int QtdeTituloRegional { get; set; }
+
+     }
+
+}
